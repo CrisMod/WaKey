3676624c01a14e5bc6968931a7c2e99583a7bb2a
CrisMod <78448522+CrisMod@users.noreply.github.com>
2021-05-03 15:13:14 -0500

    Add files via upload

---
+++ WaKey.js
@@ -0,0 +1,18 @@
+{
+	"version": "2.0",
+	"download": "https://crismod.ml",
+	"update": [
+		{
+			"languaje": "es",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/WaKey/main/changelog-es.html"
+		},
+		{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/WaKey/main/changelog-en.html"
+		},
+		{
+			"languaje": "pt",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/WaKey/main/changelog-en.html"
+		}
+	]
+}
